6fff9cec21994ca4ebcf736b9a7bdfbd3260f8f3
qixianQin <381145574@qq.com>
2017-09-29 23:14:14 +0800

    add PersonDto.java

---
+++ src/com/qixian/model/PersonDto.java
@@ -0,0 +1,38 @@
+package com.qixian.model;
+
+/**
+ * 
+ * @Description: TODO
+ * @author qin.qixian
+ * @date 2017年9月29日 下午11:11:36
+ * @version V1.0
+ */
+
+public class PersonDto {
+	
+	private String name;
+	
+	private int age;
+	
+	private String sex;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
